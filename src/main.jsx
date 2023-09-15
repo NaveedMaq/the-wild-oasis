4dc17740fe0352a45bf1d7ea6bc02241d5ea8adf
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-15 14:40:59 +0530

    add react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalStyles from './styles/GlobalStyles.js';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
