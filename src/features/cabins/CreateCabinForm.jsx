1b6eac68b831109ac1bb1d51338c16e1528493d9
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-16 16:07:05 +0530

    add image upload functionality

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -34,7 +34,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -119,7 +119,13 @@ function CreateCabinForm() {
 
       <FormRow>
         <Label htmlFor='image'>Cabin photo</Label>
-        <FileInput id='image' accept='image/*' />
+        <FileInput
+          id='image'
+          accept='image/*'
+          {...register('image', {
+            required: 'This field is required',
+          })}
+        />
       </FormRow>
 
       <FormRow>
