8f92066a3dcf46a11d397299bc4d6d0c6510ca39
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-17 18:47:58 +0530

    add useOutsideClick hook

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,7 +1,6 @@
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
 import CreateCabinForm from './CreateCabinForm';
-import CabinTable from './CabinTable';
 
 function AddCabin() {
   return (
@@ -12,31 +11,8 @@ function AddCabin() {
       <Modal.Window name='cabin-form'>
         <CreateCabinForm />
       </Modal.Window>
-
-      <Modal.Open opens='table'>
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name='table'>
-        <CabinTable />
-      </Modal.Window>
     </Modal>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddCabin;
