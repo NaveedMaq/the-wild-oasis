22afe622542db656087b68e8e885566d8df8e88f
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-19 16:38:09 +0530

    add booking details page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
+import Booking from './pages/Booking';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route index element={<Navigate replace to='/dashboard' />} />
           <Route path='dashboard' element={<Dashboard />} />
           <Route path='bookings' element={<Bookings />} />
+          <Route path='bookings/:bookingId' element={<Booking />} />
           <Route path='cabins' element={<Cabins />} />
           <Route path='users' element={<Users />} />
           <Route path='settings' element={<Settings />} />
