2ac7aef2bc678685a27b966dac026165e7b357a3
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-20 12:27:22 +0530

    add react error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import GlobalStyles from './styles/GlobalStyles.js';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'react-hot-toast';
-import { DarkModeProvider } from './context/DarkModeContext.jsx';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 0,
-    },
-  },
-});
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Toaster
-          position='top-center'
-          gutter={12}
-          containerStyle={{ margin: '8px' }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: '16px',
-              maxWidth: '500px',
-              padding: '16px 24px',
-              backgroundColor: 'var(--color-grey-0)',
-              color: 'var(--color-grey-700)',
-            },
-          }}
-        />
-        <GlobalStyles />
-        <App />
-      </QueryClientProvider>
-    </DarkModeProvider>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
